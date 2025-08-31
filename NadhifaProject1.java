91ba7396d23cfdb01a6b8da9b402ef8ef7c36206
nadhifazlfa <nazazalfa23@gmail.com>
2025-08-31 19:19:56 +0700

    Add files via upload

---
+++ NadhifaProject1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nadhifa.project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class NadhifaProject1 {
+
+    public static void main(String[] args) {
+        String nama = "Nadhifatus Zalfa Az Zanirah";
+        String npm = "2410506012";
+        String alamat = "Gunungmujil Kuwarasan Kebumen"; 
+        String no_hp = "085183867591"; 
+        
+        System.out.println("Nama  : " + nama);
+        System.out.println("NPM   : " + npm);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("No Hp : " + no_hp);
+    }
+}
